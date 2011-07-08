4bf700e0af40c2b332e9f93740047e7d5811552e
Igor Anic <ianic@minus5.hr>
2011-07-08 11:22:40 +0200

    adding required deps according to site-s documentation

--- lib/deprec/recipes/rvm.rb
+++ lib/deprec/recipes/rvm.rb
@@ -15,7 +15,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   
       task :install_deps do
         deprec::git::install
-        apt.install({:base => %w(curl)}, :stable)
+        apt.install({:base => %w(curl build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
+)}, :stable)        
       end
 
       task :install_rubies do
